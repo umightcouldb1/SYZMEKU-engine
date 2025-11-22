a7c433805ebee83756ccc2ad9f1057d632804084
umightcouldb1 <umightcouldb1@toisouljahacademy.com>
2025-11-22 07:33:41 -0600

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// This configuration is necessary to support import.meta.env in the build process.
 export default defineConfig({
   plugins: [react()],
-  // FIX: Change the build target to 'es2020' to support modern features like import.meta.env
   build: {
     target: 'es2020',
   },
