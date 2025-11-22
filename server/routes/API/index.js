9296cb5404df8b43ad6bd432e1cea992b9949cd9
umightcouldb1 <umightcouldb1@toisouljahacademy.com>
2025-11-22 09:50:00 -0600

    Update index.js

--- server/routes/API/index.js
+++ server/routes/API/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const projectRoutes = require('./projectRoutes');
+const projectRoutes = require('./projectRoutes'); 
+// const userRoutes = require('./userRoutes'); // Future use
 
 router.use('/projects', projectRoutes);
+// router.use('/users', userRoutes); // Future use
 
 module.exports = router;
