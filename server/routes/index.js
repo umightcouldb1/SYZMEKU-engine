2209d96f1a8907c1daa5b9f763304bf22766adf4
umightcouldb1 <umightcouldb1@toisouljahacademy.com>
2025-11-22 09:44:59 -0600

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const apiRoutes = require('./api'); // This is the file that Node is complaining about
 
 // All requests to /api will use the routes defined in api/index.js
 router.use('/api', apiRoutes);
