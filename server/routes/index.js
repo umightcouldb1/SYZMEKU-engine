22f4f1729521acb072688d0f5d0bb3022ef59b4c
umightcouldb1 <umightcouldb1@toisouljahacademy.com>
2025-11-22 10:00:48 -0600

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-const apiRoutes = require('./api'); 
+const apiRoutes = require('./API'); // This line is corrected to match the GitHub folder case
 
-// All requests to /api will use the routes defined in api/index.js
+// All requests to /api will use the routes defined in API/index.js
 router.use('/api', apiRoutes);
 
 module.exports = router;
