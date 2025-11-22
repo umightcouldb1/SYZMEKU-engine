62b582b3b819c8ae41d57ca2e99ac69574d25829
umightcouldb1 <umightcouldb1@toisouljahacademy.com>
2025-11-22 09:13:50 -0600

    Create index.js

---
+++ server/routes/index.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+// All requests to /api will use the routes defined in api/index.js
+router.use('/api', apiRoutes);
+
+module.exports = router;
