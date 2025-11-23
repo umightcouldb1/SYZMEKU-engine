bcbeba5011b9fef81ed4094a96477cb7982191a8
umightcouldb1 <umightcouldb1@toisouljahacademy.com>
2025-11-23 11:49:33 -0600

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css'; // CRITICAL: Ensure this is the first import
 import App from './App.jsx';
-import './index.css';
 import { AuthProvider } from './hooks/useAuth.jsx'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
