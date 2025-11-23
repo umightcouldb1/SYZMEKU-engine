c7bb8a9589d770f1bf1b694fca70a5dd3f4bb819
umightcouldb1 <umightcouldb1@toisouljahacademy.com>
2025-11-23 11:23:28 -0600

    Update index.js

--- server/routes/API/index.js
+++ server/routes/API/index.js
@@ -1,11 +1,8 @@
 const router = require('express').Router();
 const projectRoutes = require('./projectRoutes');
-const authRoutes = require('./authRoutes'); // New Import
+const authRoutes = require('./authRoutes'); 
 
-// Projects routes are prefixed with /api/projects
 router.use('/projects', projectRoutes);
-
-// Authentication routes are prefixed with /api/auth
-router.use('/auth', authRoutes); // New Auth Route
+router.use('/auth', authRoutes); 
 
 module.exports = router;
