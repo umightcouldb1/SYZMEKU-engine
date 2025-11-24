eaeda9c31bac27983d16c404dbae166f7ef62d43
umightcouldb1 <umightcouldb1@toisouljahacademy.com>
2025-11-24 05:56:16 -0600

    Update server.cjs

--- server/server.cjs
+++ server/server.cjs
@@ -3,22 +3,19 @@ const express = require('express');
 const colors = require('colors');
 const connectDB = require('./configure/db');
 const path = require('path');
-const { errorHandler } = require('C:\\Users\\umight\\SYZMEKU-engine\\server\\middleware\\errorMiddleware');
+const { errorHandler } = require('./middleware/errorMiddleware');
 
 connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware for handling raw JSON and urlencoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Define API routes
-// app.use('/api/users', require('./routes/userRoutes')); 
+// app.use('/api/users', require('./routes/userRoutes'));
 // app.use('/api/goals', require('./routes/goalRoutes'));
 
-// Serve client build folder in production
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
@@ -26,11 +23,9 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, '../', 'client', 'dist', 'index.html'))
     );
 } else {
-    // Basic route for non-production environments
     app.get('/', (req, res) => res.send('Please set to production'));
 }
 
-// Error Handler Middleware must be last
 app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`.yellow));
