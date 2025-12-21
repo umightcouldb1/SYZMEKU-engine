e8d5e902f15b7f1b22f3b665c6301dbb7523d204
umightcouldb1 <umightcouldb1@toisouljahacademy.com>
2025-12-21 13:27:44 -0600

    Fix user model import in auth middleware

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,18 +2,18 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import AuthScreen from './AuthScreen';
 import Dashboard from './Dashboard';
-import './App.css';
 
 function App() {
-  const { user } = useSelector((state) => state.auth);
+  const auth = useSelector((state) => state.auth || {});
+  const { user, isLoading } = auth;
+
+  if (isLoading) {
+    return <div style={{ color: 'gold', padding: '20px' }}>Loading Engine...</div>;
+  }
 
   return (
     <div className="App">
-      {!user ? (
-        <AuthScreen />
-      ) : (
-        <Dashboard />
-      )}
+      {!user ? <AuthScreen /> : <Dashboard />}
     </div>
   );
 }
