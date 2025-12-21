ac31dc964c6dd546df80c8045d5e78cb52f09d16
umightcouldb1 <umightcouldb1@toisouljahacademy.com>
2025-12-21 15:36:20 -0600

    Remove star glyph from cascade

--- client/src/AuthScreen.jsx
+++ client/src/AuthScreen.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const AuthScreen = () => {
   const canvasRef = useRef(null);
-  const [buttonText, setButtonText] = useState('INITIATE ASCENSION');
-  const [titleText, setTitleText] = useState('SYZMEKU ENGINE');
+  const buttonText = 'INITIATE ASCENSION';
+  const titleText = 'SYZMEKU ENGINE';
 
   useEffect(() => {
     const canvas = canvasRef.current;
@@ -24,14 +24,14 @@ const AuthScreen = () => {
     resizeCanvas();
 
     const characters =
-      'ᚐᚑᚒᚓᚔᚕᚖᚗᚘᚙᚚ᚛᚜☖☗☙☚☛☜☝☞☟☠☡☢☣☤☥☦☧☨☩☪☫☬☭☮☯☰☱☲☳☴☵☶☷☸☹☺☻☼☽☾☿♀♁♂♃♄♅♆♇♈♉♊♋♌♍♎♏♐♑♒♓♔♕♖♗♘♙♚♛♜♝♞♟';
+      'ᚙᚚ᚛᚜☖☗☰☱☲☳☴☵☶☷☸♔♕♖♗♘♙♚♛♜♝♞♟✚✙✛✜✟✠✢✣✤✥';
     const charArray = characters.split('');
     const fontSize = 16;
     const columns = Math.floor(canvas.width / fontSize);
     const drops = Array.from({ length: columns }, () => 1);
 
     const draw = () => {
-      ctx.fillStyle = 'rgba(5, 5, 15, 0.05)';
+      ctx.fillStyle = 'rgba(5, 5, 15, 0.1)';
       ctx.fillRect(0, 0, canvas.width, canvas.height);
 
       ctx.fillStyle = '#FFD700';
@@ -41,54 +41,20 @@ const AuthScreen = () => {
         const text = charArray[Math.floor(Math.random() * charArray.length)];
         ctx.fillText(text, i * fontSize, drops[i] * fontSize);
 
-        if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+        if (drops[i] * fontSize > canvas.height && Math.random() > 0.99) {
           drops[i] = 0;
         }
         drops[i] += 1;
       }
     };
 
-    const interval = setInterval(draw, 33);
+    const interval = setInterval(draw, 65);
     window.addEventListener('resize', resizeCanvas);
 
     return () => {
       clearInterval(interval);
       window.removeEventListener('resize', resizeCanvas);
     };
-  }, []);
-
-  useEffect(() => {
-    const scripts = {
-      english: 'INITIATE ASCENSION',
-      atlantean: 'ᚐᚑᚒᚓᚔᚕᚖᚗᚘᚙᚚ',
-      aurebesh: 'ᔓᔗᔘᔙᔚᔛᔜᔝᔞᔟ',
-    };
-    const keys = Object.keys(scripts);
-    let i = 0;
-
-    const interval = setInterval(() => {
-      i = (i + 1) % keys.length;
-      setButtonText(scripts[keys[i]]);
-    }, 800);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    const scripts = {
-      english: 'SYZMEKU ENGINE',
-      atlantean: 'ᚐᚑᚒᚓᚔᚕᚖᚗᚘᚙᚚ',
-      aurebesh: 'ᔓᔗᔘᔙᔚᔛᔜᔝᔞᔟ',
-    };
-    const keys = Object.keys(scripts);
-    let i = 0;
-
-    const interval = setInterval(() => {
-      i = (i + 1) % keys.length;
-      setTitleText(scripts[keys[i]]);
-    }, 800);
-
-    return () => clearInterval(interval);
   }, []);
 
   return (
