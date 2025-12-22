2551e29f1e499ef724a1b6547e2b29e480c7034b
umightcouldb1 <umightcouldb1@toisouljahacademy.com>
2025-12-22 10:42:40 -0600

    Add Codex harm filter endpoint (#37)

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 // Seal the pathways to individual logic modules
 router.use('/auth', require('./authRoutes'));
 router.use('/fixes', require('./fixesRoutes'));
+router.use('/codex-command', require('./codexRoutes'));
 
 module.exports = router;
