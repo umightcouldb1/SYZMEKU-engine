a361a5462f66b3c804b17da714ad805e930af64c
umightcouldb1 <umightcouldb1@toisouljahacademy.com>
2026-06-05 17:15:22 -0500

    style: import welcome interaction enhancements

--- client/src/WelcomeScreen.jsx
+++ client/src/WelcomeScreen.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './interactionEnhancements.css';
 
 const clearStaleAuthState = () => {
   localStorage.removeItem('user');
