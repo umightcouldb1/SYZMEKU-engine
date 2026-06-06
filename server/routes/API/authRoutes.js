774210ac1c9b2bdf2c068c0a3e82e5d7348700ef
umightcouldb1 <umightcouldb1@toisouljahacademy.com>
2026-06-06 14:25:45 -0500

    feat: mount Google OAuth auth route

--- server/routes/API/authRoutes.js
+++ server/routes/API/authRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const asyncHandler = require('express-async-handler');
 const User = require('../../models/User');
-const { registerUser, loginUser, logoutUser } = require('../../controllers/auth controller');
+const { registerUser, loginUser, googleLoginUser, logoutUser } = require('../../controllers/auth controller');
 const { protect } = require('../../middleware/authMiddleware');
 
 // @route POST /api/auth/signup
@@ -11,6 +11,9 @@ router.post('/register', asyncHandler(registerUser));
 
 // @route POST /api/auth/login
 router.post('/login', asyncHandler(loginUser));
+
+// @route POST /api/auth/google
+router.post('/google', asyncHandler(googleLoginUser));
 
 // @route POST /api/auth/logout
 router.post('/logout', protect, asyncHandler(logoutUser));
