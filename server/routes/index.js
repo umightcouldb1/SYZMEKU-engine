a4958063f0d99df3da05149cffb9ffb774d5b139
umightcouldb1 <umightcouldb1@toisouljahacademy.com>
2026-06-06 08:33:01 -0500

    feat: mount starburst core routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,6 +10,7 @@ router.use('/crystalline', require('./crystallineRoutes'));
 router.use('/scroll-intake', require('./scrollIntakeRoutes'));
 router.use('/scroll-match', require('./scrollMatchRoutes'));
 router.use('/mentor-system', require('./mentorSystemRoutes'));
+router.use('/starburst-core', require('../starburst-core/ancestralRoutes'));
 
 router.use('/core', require('./API/coreRoutes'));
 
