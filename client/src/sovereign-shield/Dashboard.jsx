bac7c914cb664d4f1fa2fc7022b8f78a422d18f4
umightcouldb1 <umightcouldb1@toisouljahacademy.com>
2026-06-06 09:42:55 -0500

    feat: surface protected admin signature on dashboard

--- client/src/sovereign-shield/Dashboard.jsx
+++ client/src/sovereign-shield/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import MentorDashboard from '../Dashboard';
+import AdminSignaturePanel from '../AdminSignaturePanel';
 import SovereignShieldPanel from './SovereignShieldPanel';
 
 export default function SovereignDashboard({ user }) {
   return (
     <div className="sovereign-shield-shell">
       <SovereignShieldPanel />
+      <AdminSignaturePanel />
       <MentorDashboard user={user} />
     </div>
   );
