4337f7a1d586f44e4045e5a9639c0b0973e22b06
umightcouldb1 <umightcouldb1@toisouljahacademy.com>
2026-06-13 08:00:21 -0500

    feat: stage academy hosting and automation bridge

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 // Seal the pathways to individual logic modules
 router.use('/auth', require('./authRoutes'));
 router.use('/profile', require('./profileRoutes'));
+router.use('/automation', require('./academyAutomationRoutes'));
 router.use('/fixes', require('./fixesRoutes'));
 router.use('/codex-command', require('./codexRoutes'));
 router.use('/mirror', require('./mirrorRoutes'));
