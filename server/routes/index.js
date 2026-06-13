46e25bbaed9c6cb9432806d6fad510a50513178e
umightcouldb1 <umightcouldb1@toisouljahacademy.com>
2026-06-12 20:24:07 -0500

    feat: add user profile management

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 // Seal the pathways to individual logic modules
 router.use('/auth', require('./authRoutes'));
+router.use('/profile', require('./profileRoutes'));
 router.use('/fixes', require('./fixesRoutes'));
 router.use('/codex-command', require('./codexRoutes'));
 router.use('/mirror', require('./mirrorRoutes'));
