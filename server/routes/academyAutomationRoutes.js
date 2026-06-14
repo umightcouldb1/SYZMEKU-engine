060496cf33ff64597425e6498e6f8607c0cda0cf
umightcouldb1 <umightcouldb1@toisouljahacademy.com>
2026-06-13 20:59:10 -0500

    Expose commander automation logs

--- server/routes/academyAutomationRoutes.js
+++ server/routes/academyAutomationRoutes.js
@@ -3,12 +3,14 @@ const { protect } = require('../middleware/authMiddleware');
 const { requireCommanderInChief } = require('../middleware/checkRole');
 const {
   getAutomationStatus,
+  listAutomationLogs,
   generatePartnershipDeck,
   generateOutreachDraft,
 } = require('../controllers/academyAutomationController');
 
 router.use(protect, requireCommanderInChief);
 router.get('/status', getAutomationStatus);
+router.get('/logs', listAutomationLogs);
 router.post('/partnership/deck-draft', generatePartnershipDeck);
 router.post('/partnership/outreach-draft', generateOutreachDraft);
 
